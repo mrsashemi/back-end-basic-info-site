fc9a56d6040ef316d56b453e3cf149fee7b84ee5
mrsashemi <mrsashemi@gmail.com>
2022-09-20 20:39:51 -0700

    Update to express

--- index.js
+++ index.js
@@ -1,40 +1,24 @@
-const http = require('http');
-const fs = require('fs');
+const express = require("express");
+const app = express();
 
-const port = process.env.PORT || 8080
+const port = 8080;
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        fs.readFile('index.html', (err, file) => {
-            if (err) throw err;
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'text/html')
-            return res.end(file);
-        })
-    } else if (req.url === "/about") {
-        fs.readFile('about.html', (err, file) => {
-            if (err) throw err;
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'text/html')
-            return res.end(file);
-        })
-    } else if (req.url === "/contact") {
-        fs.readFile('contact-me.html', (err, file) => {
-            if (err) throw err;
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'text/html')
-            return res.end(file);
-        })
-    } else {
-        fs.readFile('404.html', (err, file) => {
-            if (err) throw err;
-            res.statusCode = 200
-            res.setHeader('Content-Type', 'text/html')
-            return res.end(file);
-        })
-    }
-})
+app.get('/',function(req,res) {
+    res.sendFile(__dirname + '/index.html');
+});
 
-server.listen(port, () => {
+app.get('/about',function(req,res) {
+    res.sendFile(__dirname + '/about.html');
+});
+
+app.get('/contact',function(req,res) {
+    res.sendFile(__dirname + '/contact-me.html');
+});
+
+app.get('/404',function(req,res) {
+    res.sendFile(__dirname + '/404.html');
+});
+
+app.listen(port, () => {
     console.log(`Server running at port ${port}`)
 })
